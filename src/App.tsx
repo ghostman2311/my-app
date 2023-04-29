326a229700d65c38d8eef41b9d614593abfe559c
ghostman2311 <78917858+ghostman2311@users.noreply.github.com>
2023-04-29 08:39:08 +0000

    Button generics implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+interface ButtonProps<T>{
+  onClickHandler: (value: T) => void,
+  label: string,
+  data:T,
+}
+
+
+const Button = <T extends unknown>(props:ButtonProps<T>) => {
+
+  const {onClickHandler,data,  label} = props
+
+  const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onClickHandler(data)
+  }
+  return <button onClick={buttonHandler}>{label}</button>
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Intermediaate Typescript</h1>
+      <Button label='submit' data={['123']} onClickHandler={(data) => {
+        console.log('here')
+        console.log(data)
+      }}></Button>
     </div>
   );
 }
