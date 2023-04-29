c635a4286a890d3cbd3cb3cb45e2cca224e2c120
ghostman2311 <78917858+ghostman2311@users.noreply.github.com>
2023-04-29 13:10:24 +0000

    hoc-1 implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
-import React from 'react';
 import './App.css';
+import ClickCounter from './components/ClickCounter';
+import { withTimer } from './components/withCounter';
 
-interface ButtonProps<T>{
-  onClickHandler: (value: T) => void,
-  label: string,
-  data:T,
-}
-
-
-const Button = <T extends unknown>(props:ButtonProps<T>) => {
-
-  const {onClickHandler,data,  label} = props
-
-  const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
-    onClickHandler(data)
-  }
-  return <button onClick={buttonHandler}>{label}</button>
-}
-
+const ClickCounterWithTimer = withTimer(ClickCounter)
 
 function App() {
   return (
     <div className="App">
       <h1>Intermediaate Typescript</h1>
-      <Button label='submit' data={['123']} onClickHandler={(data) => {
-        console.log('here')
-        console.log(data)
-      }}></Button>
+      <ClickCounterWithTimer />
     </div>
   );
 }
